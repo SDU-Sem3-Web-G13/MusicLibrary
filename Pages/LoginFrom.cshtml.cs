50a1bb91fb5110ce43f7af8fdafb7c9300fd7668
Tokushiro <oskarandrzejewski16@gmail.com>
2024-10-02 15:40:40 +0200

    Finished Default page as well as additional login and register page. By Hana and me.

---
+++ Pages/LoginFrom.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorMusic.Pages
+{
+    public class LoginFromModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/RegisterForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorMusic.Pages
+{
+    public class RegisterFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
