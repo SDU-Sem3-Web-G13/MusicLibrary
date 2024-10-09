5a6187707414ea37881c02016908cc22dc6822d0
Patrik Muravski <pamur24@student.sdu.dk>
2024-10-09 17:11:08 +0300

    added javascrip on add and delete button

---
+++ wwwroot/js/formPage.js
@@ -0,0 +1,25 @@
+﻿const listContainer = document.getElementById("list-container");
+
+function addTrack() {
+    if (listContainer.childElementCount === 10) {
+        alert("List can have max 10 elements");
+    }
+    else {
+        let li = document.createElement("li");
+        let input = document.createElement('input');
+        input.type = 'text';
+
+        li.appendChild(input);
+        listContainer.appendChild(li);
+    }
+}
+
+function deleteTrack() {
+    if (listContainer.childElementCount === 1) {
+        alert("Has to be at least one track");
+    }
+    else {
+        const li = listContainer.querySelector('li:last-child');
+        li.remove();
+    }
+}
