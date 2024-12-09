b04d9ab9cf066e1db123456099bd82640a9fbe52
Nick <niklas.braun30419@gmail.com>
2024-12-09 22:22:37 +0100

    Moving Scripts and Styles into wwwroot; proper HTML document template on each page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function Logout() {
+    $.ajax({
+        url: '/Index?handler=Logout',
+        type: 'GET',
+        async: false,
+        success: function(response) {
+            if (response.success) {
+                window.location.href = '/Login';
+            } else {
+                console.error('Failed to logout');
+            }
+        },
+        error: function() {
+            console.error('Error in AJAX request');
+        }
+    });
+}
